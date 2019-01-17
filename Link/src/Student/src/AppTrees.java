fdbab82dca1228502042381c4b03d0bdca9757be
DarkRecluse <46132422+DarkRecluse@users.noreply.github.com>
2019-01-18 01:00:14 +0700

    Change bit

---
+++ Link/src/Student/src/AppTrees.java
@@ -0,0 +1,13 @@
+
+public class AppTrees {
+	public static void main (String args[]) {
+		Trees example = new Trees();
+		example.insert("4478", "Rose", "Mathmetics", "A");
+		example.insert("4479", "Elsword", "Mathmetics", "C+");
+		example.insert("4480", "Aisha", "Mathmetics", "B+");
+	
+		
+	
+	}
+
+}
