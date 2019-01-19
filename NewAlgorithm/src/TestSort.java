1458fe1dafe9cfa4e186398e529aa4bd0e7270a5
DarkRecluse <46132422+DarkRecluse@users.noreply.github.com>
2019-01-19 20:32:09 +0700

    Fixed

--- NewAlgorithm/src/TestSort.java
+++ NewAlgorithm/src/TestSort.java
@@ -3,46 +3,53 @@
 public class TestSort {
 
 	public static void main(String[] args) {
-		CodeTime Time = new CodeTime();
-		int numberRun = 10000;		
+		long endTime = 0;
+		long startTime = 0;
+		long sumBubble = 0;
+		long sumSelection = 0;
+		long sumInsertion = 0;
+		int numberRun = 4000;		
 		int  ranDomNub [] = new int [numberRun];	
 		
 		BubbleSort bubble = new BubbleSort(numberRun);
 		InsertionSort insertion = new InsertionSort(numberRun);
 		SelectionSort selection = new SelectionSort(numberRun);
 		
-				
-		//Random number
-		for (int i = 0;i>ranDomNub.length;i++) {
-			ranDomNub [i] = (int)(Math.random()*10000);
-		}
 		
-		//Start all sort 
-		
-		for (int i = 0;i>numberRun;i++) {
+		for (int i = 0; i < ranDomNub.length; i++) {
+			ranDomNub[i] = (int) (Math.random() * 10000);
 			bubble.insert(ranDomNub[i]);
 			insertion.insert(ranDomNub[i]);
 			selection.insert(ranDomNub[i]);
 		}
-				
-		Time.startTime();
-		bubble.sort();
-		Time.stopTime();
-		System.out.println("Time for BubbleSort : " + Time.getActionTime());
-		Time.resetTime();
-		
-		Time.startTime();
-		insertion.sort();
-		Time.stopTime();
-		System.out.println("Time for InsertionSort : " + Time.getActionTime());
-		Time.resetTime();
-		
-		Time.startTime();
-		selection.sort();
-		Time.stopTime();
-		System.out.println("Time for SelectionSort : " + Time.getActionTime());
-		Time.resetTime();
-	
-	}
 
+		
+		for(int i = 0;i<100;i++) {
+		startTime = System.nanoTime();
+		bubble.sort();
+		endTime = System.nanoTime();
+		sumBubble = sumBubble + (endTime - startTime);
+			
+		startTime = System.nanoTime();
+		insertion.sort();
+		endTime = System.nanoTime();
+		sumInsertion = sumInsertion + (endTime - startTime);
+		
+		
+			
+		startTime = System.nanoTime();
+		selection.sort();// sorting array elements using bubble sort
+		endTime = System.nanoTime();		
+		sumSelection = sumSelection + (endTime - startTime);
+		
+		
+		
+		
+		}
+		System.out.println("Time After Bubble Sort : " + (sumBubble / 100) + " ns");
+		System.out.println("Time After Insertion Sort : " + (sumInsertion / 100) + " ns");
+		System.out.println("Time Selection Sort : " + (sumSelection / 100) + " ns");
+		
+		
+	}
 }
